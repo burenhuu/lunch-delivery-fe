e0e7b155bae75da44ff480441c002b67261600c5
burenhu <bburenhuu@gmail.com>
2023-01-08 17:12:32 +0800

    01-08 burne

--- src/components/category/product-tab.tsx
+++ src/components/category/product-tab.tsx
@@ -9,26 +9,27 @@ export default function ProductTab({
 }) {
     return (
         <div className="flex items-center justify-between px-5 gap-x-1.25 ">
-            {productFilters?.map((filter: any) => {
+            {productFilters?.map((filter: any, index: any) => {
                 return (
                     <>
                         {
                             activeTab.sort === filter.sort ?
                                 <div
-                                    key={filter.sort}
+                                    key={filter.sort + index}
                                     className={
-                                        "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-main gradient-border"
+                                        "bg-white relative w-full text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-main gradient-border"
                                     }
                                 >
                                     {filter.name}
                                 </div>
                                 :
                                 <div
-                                    key={filter.sort}
+                                    key={filter.sort + index}
                                     onClick={() => setActiveTab(filter)}
                                     className={
-                                        "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-gray"
+                                        "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5"
                                     }
+                                    style={{color: 'rgba(100, 115, 130, 1)'}}
                                 >
                                     {filter.name}
                                 </div>

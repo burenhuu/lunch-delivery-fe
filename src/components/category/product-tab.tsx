e3fc41ca36e2f700a2ba9fd5af3ec1dd1e2a465f
burenhu <bburenhuu@gmail.com>
2023-01-08 17:04:17 +0800

    01-08 burne

--- src/components/category/product-tab.tsx
+++ src/components/category/product-tab.tsx
@@ -11,18 +11,31 @@ export default function ProductTab({
         <div className="flex items-center justify-between px-5 gap-x-1.25 ">
             {productFilters?.map((filter: any) => {
                 return (
-                    <div
-                        key={filter.sort}
-                        onClick={() => setActiveTab(filter)}
-                        className={
-                            "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 " +
-                            (activeTab.sort === filter.sort
-                                ? "text-main gradient-border"
-                                : "text-gray")
+                    <>
+                        {
+                            activeTab.sort === filter.sort ?
+                                <div
+                                    key={filter.sort}
+                                    className={
+                                        "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-main gradient-border"
+                                    }
+                                >
+                                    {filter.name}
+                                </div>
+                                :
+                                <div
+                                    key={filter.sort}
+                                    onClick={() => setActiveTab(filter)}
+                                    className={
+                                        "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-gray"
+                                    }
+                                >
+                                    {filter.name}
+                                </div>
                         }
-                    >
-                        {filter.name}
-                    </div>
+
+                    </>
+
                 );
             })}
         </div>

5430d7ca2772aaed74b6dceba3c22fcdbd557af6
nearUNB <near1111.nn@gmail.com>
2023-01-08 17:22:38 +0800

    Update

--- src/components/category/product-tab.tsx
+++ src/components/category/product-tab.tsx
@@ -1,42 +1,38 @@
 export default function ProductTab({
     activeTab,
     setActiveTab,
-    productFilters
+    productFilters,
 }: {
     activeTab: any;
     setActiveTab: any;
-    productFilters: any
+    productFilters: any;
 }) {
     return (
         <div className="flex items-center justify-between px-5 gap-x-1.25 ">
-            {productFilters?.map((filter: any, index: any) => {
+            {productFilters?.map((filter: any) => {
                 return (
                     <>
-                        {
-                            activeTab.sort === filter.sort ?
-                                <div
-                                    key={filter.sort + index}
-                                    className={
-                                        "bg-white relative w-full text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-main gradient-border"
-                                    }
-                                >
-                                    {filter.name}
-                                </div>
-                                :
-                                <div
-                                    key={filter.sort + index}
-                                    onClick={() => setActiveTab(filter)}
-                                    className={
-                                        "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5"
-                                    }
-                                    style={{color: 'rgba(100, 115, 130, 1)'}}
-                                >
-                                    {filter.name}
-                                </div>
-                        }
-
+                        {activeTab.sort === filter.sort ? (
+                            <div
+                                key={filter.sort}
+                                className={
+                                    "bg-white relative w-full  text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-main gradient-border"
+                                }
+                            >
+                                {filter.name}
+                            </div>
+                        ) : (
+                            <div
+                                key={filter.sort}
+                                onClick={() => setActiveTab(filter)}
+                                className={
+                                    "bg-white w-full text-center bg-clip-padding text-sm rounded-md z-10 shadow-delivery p-2.5 text-gray"
+                                }
+                            >
+                                {filter.name}
+                            </div>
+                        )}
                     </>
-
                 );
             })}
         </div>

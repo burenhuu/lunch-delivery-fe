28d7189d97355eae04f086b9b9409342a40054ba
nb <nb685858@gmail.com>
2023-01-13 11:56:29 +0800

    update

--- src/components/history/review.tsx
+++ src/components/history/review.tsx
@@ -85,201 +85,189 @@ export default function Review({
     }
 
     return (
-        <>
-            <Drawer
-                open={showDrawer}
-                onClose={toggleDrawer}
-                direction="bottom"
-                enableOverlay={true}
-                style={{
-                    background: "#FFFFFF",
-                    height: liked === "" ? "379px" : "687px",
-                }}
-                overlayOpacity={0.5}
-                overlayColor="#1e2335"
-                className={`p-5 rounded-t-[20px] relative`}
-            >
-                <div className="drag-indicator" onTouchEnd={toggleDrawer}></div>
-                <div className="rounded-2xl min-h-[160px] overflow-hidden shadow-delivery relative mb-[20px]">
-                    <img
-                        src={item.merchant.logo}
-                        className="h-40 min-w-full"
-                        alt={item.merchant.name}
-                    />
-                    <div className="absolute z-20 left-3.75 bottom-3.75 text-white my-col-5 items-start">
-                        <div className="text-sm">{`${item.merchant.name} ( ${item.merchant.distance} км )`}</div>
-                    </div>
+        <Drawer
+            open={showDrawer}
+            onClose={toggleDrawer}
+            direction="bottom"
+            enableOverlay={true}
+            style={{
+                background: "#FFFFFF",
+                height: liked === "" ? "379px" : "687px",
+            }}
+            overlayOpacity={0.5}
+            overlayColor="#1e2335"
+            className={`p-5 rounded-t-[20px] relative`}
+        >
+            <div className="drag-indicator" onTouchEnd={toggleDrawer}></div>
+            <div className="rounded-2xl min-h-[160px] overflow-hidden shadow-delivery relative mb-[20px]">
+                <img
+                    src={item.merchant.logo}
+                    className="h-40 min-w-full"
+                    alt={item.merchant.name}
+                />
+                <div className="absolute z-20 left-3.75 bottom-3.75 text-white my-col-5 items-start">
+                    <div className="text-sm">{`${item.merchant.name} ( ${item.merchant.distance} км )`}</div>
+                </div>
+            </div>
+
+            <div className="font-medium text-center">
+                {type === "S" ? (
+                    <>
+                        Танд хоолны амт,
+                        <div>чанар таалагдсан уу?</div>
+                    </>
+                ) : (
+                    <>Хүргэлтийн үйлчилгээ таалагдсан уу?</>
+                )}
+            </div>
+            <div>
+                <div className="flex flex-row justify-center py-5 gap-x-[45px]">
+                    {emojis.map((emoji: any, index: any) => (
+                        <div
+                            key={index}
+                            className={`rounded-full${
+                                liked === index
+                                    ? " bg-gradient-to-r from-[#ffbe78] to-[#ff7a1f]"
+                                    : ""
+                            }`}
+                            style={{ padding: "2px" }}
+                            onClick={() => setLiked(index)}
+                        >
+                            <div className="rounded-full w-[53px] h-[53px] text-[25px] bg-[#F5F5FA] flex items-center justify-center">
+                                {emoji}
+                            </div>
+                        </div>
+                    ))}
                 </div>
 
-                <div className="font-medium text-center">
-                    {type === "S" ? (
-                        <>
-                            Танд хоолны амт,
-                            <div>чанар таалагдсан уу?</div>
-                        </>
-                    ) : (
-                        <>Хүргэлтийн үйлчилгээ таалагдсан уу?</>
-                    )}
-                </div>
-                <div>
-                    <div className="flex flex-row justify-center py-5 gap-x-[45px]">
-                        {emojis.map((emoji: any, index: any) => (
-                            <div
-                                key={index}
-                                className={`rounded-full${
-                                    liked === index
-                                        ? " bg-gradient-to-r from-[#ffbe78] to-[#ff7a1f]"
-                                        : ""
-                                }`}
-                                style={{ padding: "2px" }}
-                                onClick={() => setLiked(index)}
-                            >
-                                <div className="rounded-full w-[53px] h-[53px] text-[25px] bg-[#F5F5FA] flex items-center justify-center">
-                                    {emoji}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                {liked !== "" && (
+                    <>
+                        <div className="font-normal pb-[15px]">
+                            {liked === 0
+                                ? "Сайжруулах зүйл нь юу байсан бэ?"
+                                : "Юу нь таалагдсан бэ?"}
+                        </div>
 
-                    {liked !== "" && (
-                        <>
-                            <div className="font-normal pb-[15px]">
-                                {liked === 0
-                                    ? "Сайжруулах зүйл нь юу байсан бэ?"
-                                    : "Юу нь таалагдсан бэ?"}
-                            </div>
-
-                            <div className="flex flex-row justify-between gap-x-[10px]">
-                                {commentChoices.map((choice: any) => (
-                                    <div
-                                        key={choice}
-                                        className={`rounded-[10px]${
-                                            comment.includes(choice)
-                                                ? " bg-gradient-to-r from-[#ffbe78] to-[#ff7a1f]"
-                                                : ""
-                                        }`}
-                                        style={{ padding: "2px" }}
-                                        onClick={() => {
-                                            if (!comment.includes(choice)) {
-                                                setComment([
-                                                    ...comment,
-                                                    choice,
-                                                ]);
-                                            } else {
-                                                setComment(
-                                                    comment.filter(
-                                                        (e: any) => e !== choice
-                                                    )
-                                                );
-                                            }
-                                        }}
-                                    >
-                                        <div className="rounded-[10px] h-[33px] bg-[#F5F5FA] flex items-center justify-center font-light  px-[15px]">
-                                            {choice}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                            {errorComment && (
-                                <p className="mt-1 text-xs italic text-left text-red-500">
-                                    Сонгоно уу
-                                </p>
-                            )}
-
-                            <div className="font-normal pb-[15px] pt-[20px]">
-                                Нэмэлт санал хүсэлт
-                            </div>
-
-                            <input
-                                type="text"
-                                placeholder="Нэмэлт тайлбар оруулах"
-                                className="bg-[#F5F5FA] text-sm font-light rounded-md px-5 py-[9px] w-full h-[35px]"
-                                onChange={handleChange}
-                            />
-
-                            <div className="mt-[10px] mb-[20px]">
-                                <div className="flex flex-wrap flex-row gap-[10px]">
-                                    {images.map((image, index) => {
-                                        return (
-                                            <div
-                                                key={index}
-                                                className="relative"
-                                            >
-                                                <img
-                                                    onClick={handleImage}
-                                                    src={image}
-                                                    className="w-[60px] h-[60px] rounded-[10px]"
-                                                />
-                                                <div
-                                                    className="absolute top-[-5px] right-[-5px]"
-                                                    onClick={() => {
-                                                        setImages(
-                                                            images.filter(
-                                                                (_, i) =>
-                                                                    i !== index
-                                                            )
-                                                        );
-                                                    }}
-                                                >
-                                                    <Remove />
-                                                </div>
-                                            </div>
-                                        );
-                                    })}
-
-                                    <img
-                                        onClick={handleImage}
-                                        src="/images/add-photos.png"
-                                        className="w-[60px] h-[60px]"
-                                    />
-                                </div>
-
+                        <div className="flex flex-row justify-between gap-x-[10px]">
+                            {commentChoices.map((choice: any) => (
                                 <div
-                                    className={`absolute transform translate-x-1/2 translate-y-1/2 right-1/2 bottom-1/2 w-full z-max ${
-                                        showCamera ? "block" : "hidden"
+                                    key={choice}
+                                    className={`rounded-[10px]${
+                                        comment.includes(choice)
+                                            ? " bg-gradient-to-r from-[#ffbe78] to-[#ff7a1f]"
+                                            : ""
                                     }`}
+                                    style={{ padding: "2px" }}
+                                    onClick={() => {
+                                        if (!comment.includes(choice)) {
+                                            setComment([...comment, choice]);
+                                        } else {
+                                            setComment(
+                                                comment.filter(
+                                                    (e: any) => e !== choice
+                                                )
+                                            );
+                                        }
+                                    }}
                                 >
-                                    <Camera
-                                        isMaxResolution={true}
-                                        sizeFactor={1}
-                                        onTakePhoto={(dataUri: any) => {
-                                            handleTakePhoto(dataUri);
-                                        }}
-                                        isFullscreen={false}
-                                        idealFacingMode="FACING_MODES.ENVIRONMENT"
-                                    />
+                                    <div className="rounded-[10px] h-[33px] bg-[#F5F5FA] flex items-center justify-center font-light  px-[15px]">
+                                        {choice}
+                                    </div>
                                 </div>
+                            ))}
+                        </div>
+                        {errorComment && (
+                            <p className="mt-1 text-xs italic text-left text-red-500">
+                                Сонгоно уу
+                            </p>
+                        )}
 
-                                {showCamera && (
-                                    <label
-                                        onClick={handleImage}
-                                        className="rounded-t-[20px]"
-                                        style={{
-                                            backgroundColor: "rgb(30, 35, 53)",
-                                            opacity: "0.5",
-                                            zIndex: "100",
-                                            height: "100vh",
-                                            left: 0,
-                                            position: "fixed",
-                                            top: 0,
-                                            width: "100%",
-                                        }}
-                                    ></label>
-                                )}
-                            </div>
+                        <div className="font-normal pb-[15px] pt-[20px]">
+                            Нэмэлт санал хүсэлт
+                        </div>
 
-                            <div className="flex justify-center w-full">
-                                <ButtonComponent
-                                    onClick={handleReview}
-                                    text="Болсон"
-                                    loading={loading}
-                                    additionalClass="max-w-[270px] w-full"
+                        <input
+                            type="text"
+                            placeholder="Нэмэлт тайлбар оруулах"
+                            className="bg-[#F5F5FA] text-sm font-light rounded-md px-5 py-[9px] w-full h-[35px]"
+                            onChange={handleChange}
+                        />
+
+                        <div className="mt-[10px] mb-[20px]">
+                            <div className="flex flex-wrap flex-row gap-[10px]">
+                                {images.map((image, index) => {
+                                    return (
+                                        <div key={index} className="relative">
+                                            <img
+                                                onClick={handleImage}
+                                                src={image}
+                                                className="w-[60px] h-[60px] rounded-[10px]"
+                                            />
+                                            <div
+                                                className="absolute top-[-5px] right-[-5px]"
+                                                onClick={() => {
+                                                    setImages(
+                                                        images.filter(
+                                                            (_, i) =>
+                                                                i !== index
+                                                        )
+                                                    );
+                                                }}
+                                            >
+                                                <Remove />
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+
+                                <img
+                                    onClick={handleImage}
+                                    src="/images/add-photos.png"
+                                    className="w-[60px] h-[60px]"
                                 />
                             </div>
-                        </>
-                    )}
-                </div>
-            </Drawer>
-        </>
+
+                            <div
+                                className={`absolute transform translate-x-1/2 translate-y-1/2 right-1/2 bottom-1/2 w-full z-max ${
+                                    showCamera ? "block" : "hidden"
+                                }`}
+                            >
+                                <Camera
+                                    onTakePhoto={(dataUri: any) => {
+                                        handleTakePhoto(dataUri);
+                                    }}
+                                    idealFacingMode="FACING_MODES.ENVIRONMENT"
+                                />
+                            </div>
+
+                            {showCamera && (
+                                <label
+                                    onClick={handleImage}
+                                    className="rounded-t-[20px] z-max"
+                                    style={{
+                                        backgroundColor: "rgb(30, 35, 53)",
+                                        opacity: "0.5",
+                                        height: "100vh",
+                                        left: 0,
+                                        position: "fixed",
+                                        top: 0,
+                                        width: "100%",
+                                    }}
+                                ></label>
+                            )}
+                        </div>
+
+                        <div className="flex justify-center w-full">
+                            <ButtonComponent
+                                onClick={handleReview}
+                                text="Болсон"
+                                loading={loading}
+                                additionalClass="max-w-[270px] w-full"
+                            />
+                        </div>
+                    </>
+                )}
+            </div>
+        </Drawer>
     );
 }
